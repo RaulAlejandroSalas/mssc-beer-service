e33c1b48e5fe002024be960338ae83cd3ebac1f6
alejandro salas <alejandrosalas0891@gmail.com>
2020-08-19 21:56:31 +0200

    [ADD] Initial Commit #1

---
+++ src/test/java/de/rauldev/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rauldev.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
